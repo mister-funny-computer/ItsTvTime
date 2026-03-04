4034e9113761945a4755d05255f28f0693af3842
Mister funny computer <tironasaurusnik@gmail.com>
2026-03-04 20:23:34 +0300

    can move question from views.py to html

--- core/views.py
+++ core/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    question = {
+        "title": "Вопрос 1",
+        "text": "Столица Франции?",
+        "answers": ["Париж", "Москва", "Мадрид", "Нью-Йорк"]
+    }
+    return render(request, "index.html", {"question": question})
+
 
