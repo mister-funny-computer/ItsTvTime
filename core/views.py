71d5c21b64d9da6f291f5ced166fb4cead31008f
Mister funny computer <tironasaurusnik@gmail.com>
2026-03-05 19:58:43 +0300

    add checking answer

--- core/views.py
+++ core/views.py
@@ -7,8 +7,19 @@ def index(request):
     question = {
         "title": "Вопрос 1",
         "text": "Столица Франции?",
-        "answers": ["Париж", "Москва", "Мадрид", "Нью-Йорк"]
+        "answers": ["Париж", "Москва", "Мадрид", "Нью-Йорк"],
+        "corect": "Париж"
     }
-    return render(request, "index.html", {"question": question})
+
+    context = {"question": question}
+
+    if request.method == "POST":
+        user_answer = request.POST.get("answer")
+        if user_answer == question["corect"]:
+            context["result"] = "Правильно!!!"
+        else:
+            context["result"] = f"Неправильно. Правильный ответ: {question["corect"]}"
+
+    return render(request, "index.html", context )
 
 
